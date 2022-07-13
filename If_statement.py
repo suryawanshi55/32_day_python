0f48f8d8fdb712c2cd2c483ba68b4e67790e225d
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-13 11:04:12 +0530

    Add files via upload

---
+++ If_statement.py
@@ -0,0 +1,85 @@
+# a=int(input("enter the value of a:"))           #enter the value of a:8
+# b=int(input("enter the value of b:"))           #enter the value of b:6
+# a,b=[int(x)for x in input().split()]
+# print(a,b)
+# a=int(input("enter the value of a:"))      
+# if( a==5):                                    # enter the value of a:5
+#     print("yes Equal")                             ## yes Equal
+# a=int(input("enter the value of a:"))    # enter the value of a:4
+# if( a==5):                                (nothing will come)      
+#     print("yes Equal")                 
+# a,b=[int(x) for x in input().split()]    #5 7       
+# if(a>b):
+#     print(f"{a}>{b} is {a>b}")
+# else:
+#     print(f"{a}>{b} is {a>b}")      #5>7 is False
+# a,b=[int(x) for x in input().split()]      #  7 4 
+# if(a>b):
+#     print(f"{a}>{b} is {a>b}")         #7>4 is True
+# else:
+#     print(f"{a}>{b} is {a>b}")
+# a,b=[int(x)for x in input().split()]    # 8 9
+# if (a>b): print("a is greater than b")
+# if (a<b): print("a is less than b")      # a is less than b
+# if (a<=b): print("a is less than or equal to b")   # a is less than or equal to b
+# if (a>=b): print("a is greater than or equal to b")
+# if (a!=b): print("a is not equal to b")           # a is not equal to b
+# if (a==b): print("a is equal to or is same as b")
+
+
+# a,b=[int(x)for x in input().split()]    #7 5
+# if (a>b): print("a is greater than b")   # a is greater than b
+# if (a<b): print("a is less than b")      
+# if (a<=b): print("a is less than or equal to b")   
+# if (a>=b): print("a is greater than or equal to b")  #a is greater than or equal to b
+# if (a!=b): print("a is not equal to b")           #a is not equal to b
+# if (a==b): print("a is equal to or is same as b")
+
+# a=int(input("enter the value of a:"))      # enter the value of a:3
+# b=int(input("enter the value of b:"))      # enter the value of b:7
+# if(a<b):
+#     print(f"{a}<{b} is True")        3<7 is True
+
+# a=int(input("enter the value of a:"))   #enter the value of a:3
+# b=int(input("enter the value of b:"))   # enter the value of b:5
+# if(a<b):
+#     print(f"{a} < {b} is true")         # 3 < 5 is true
+#     print("hey python")                 # hey python
+#     print("its done")                   # its done
+
+# a=int(input("enter the value of a:"))   
+# b=int(input("enter the value of b:"))   
+# if(a<b):                                      # enter the value of a:5
+#     print(f"{a} < {b} is true")                #enter the value of b:3
+#     print("hey python")                 
+#     print("its done")                
+
+# a=int(input("enter the value of a:")) 
+# b=int(input("enter the value of b:")) 
+# if(a<b):
+#     print(f"{a} < {b} is true")       # 4 < 8 is true
+# if(a<=b):
+#     print(f"{a} < {b} is true")       #4 < 8 is true
+# if(a>b):
+#     print(f"{a} < {b} is true")
+# if(a>=b):
+#     print(f"{a} < {b} is true")
+# if(a==b):
+#     print(f"{a} < {b} is true")
+# n=int(input("enter any intiger value"))
+# if(n%2==0):
+#     print(f"{n} is an Even number")     # enter any intiger value4
+# else:
+#     print(f"{n} is an odd number")      # 4 is an Even number
+
+
+
+# n=int(input("enter any intiger value"))       #enter any intiger value5
+# if(n%2==0):
+#     print(f"{n} is an Even number")           #5 is an odd number
+# else:
+#     print(f"{n} is an odd number")
+
+    
+
+  
